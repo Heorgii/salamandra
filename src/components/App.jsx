be1e18dadb8c6377f042b19fa592798bb43bfbbb
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-09-21 09:39:06 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import React from 'react';
+import { Header } from './Header/Header';
+import { Menu } from './Menu/Menu';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Header />
+      <Menu />
     </div>
   );
 };
