3180ba010d708689db544263922d4faaed7b249c
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-09-25 13:35:50 +0300

    fix

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -10,9 +10,10 @@ import {
   NavListItemLink,
 } from './Navigation.styled';
 
-export const Navigation = ({catalog, isLoading, error}) => {
+export const Navigation = ({ catalog, isLoading, error }) => {
   const [, setScrollX] = useState(0); //scrollX
-  const [isFixed, setIsFixed] = useState("");
+  const [isFixed, setIsFixed] = useState(false);
+
   const handleSliderScroll = e => {
     const container = e.target;
     const scrollLeft = container.scrollLeft;
@@ -27,7 +28,7 @@ export const Navigation = ({catalog, isLoading, error}) => {
       if (!nav) return;
 
       const navPosition = nav.getBoundingClientRect().top;
-      setIsFixed((navPosition <= 0));
+      setIsFixed(navPosition <= 0);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -39,7 +40,7 @@ export const Navigation = ({catalog, isLoading, error}) => {
   return (
     <NavBox id="nav">
       <SwitchTheme />
-      <Nav onScroll={handleSliderScroll} isfixed={isFixed}>
+      <Nav onScroll={handleSliderScroll} isFixed={isFixed}>
         <NavList>
           {isLoading ? onLoading() : onLoaded()}
           {error && onFetchError('Whoops, something went wrong')}
@@ -55,4 +56,3 @@ export const Navigation = ({catalog, isLoading, error}) => {
     </NavBox>
   );
 };
-
