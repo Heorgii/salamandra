74fb04748a3fd3164e4bc62e81c2034b65295a17
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-10-04 18:33:27 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={'Loading'} persistor={persistor}>
-        <BrowserRouter basename="salamandra">
+        {/* salamandra */}
+        <BrowserRouter basename="/">
           <ThemeStatus>
             <GlobalStyle />
             <App />
